dc31c8bc36c2a0c2795815a8587f94a85000954a
netlab user <netlabuser@cnislab.dlsu.edu.ph>
2016-01-30 11:29:50 +0800

    updated

--- contribution-a.c
+++ contribution-a.c
@@ -3,13 +3,10 @@
 int main() {
 	printf("CODE BY INT-ARGC!");
 	
-<<<<<<< HEAD
-	
-	printf ("MADE BY HIKARI <3333");
-	
-=======
+
+	printf ("CODE BY STRING_ARGS");
+
 	int newContrib = 1;
 	printf("Person A as new contribution : %d\n", newContrib);
->>>>>>> 02718d33a7bfcd346c5481da1aef767932d21602
 	return 0;
 }
